d8aaed83a0fd9359593f7f0b1188f35c71a1b455
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-06-20 07:58:04 -0500

    ATRIBUTOS - Clase Nieto

--- src/Nieto.java
+++ src/Nieto.java
@@ -1,3 +1,26 @@
 public class Nieto {
     //ATRIBUTOS
+    int pecas;
+    String color_pecas;
+
+    public Nieto(int pecas, String color_pecas) {
+        this.pecas = pecas;
+        this.color_pecas = color_pecas;
+    }
+
+    public int getPecas() {
+        return pecas;
+    }
+
+    public void setPecas(int pecas) {
+        this.pecas = pecas;
+    }
+
+    public String getColor_pecas() {
+        return color_pecas;
+    }
+
+    public void setColor_pecas(String color_pecas) {
+        this.color_pecas = color_pecas;
+    }
 }
