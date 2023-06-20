16c7ba870274ea80d1b150d8aca72ea980fc3c54
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-06-20 07:57:56 -0500

    ATRIBUTOS - Clase Nieta

--- src/Nieta.java
+++ src/Nieta.java
@@ -1,3 +1,26 @@
 public class Nieta {
     //ATRIBUTOS
+    String forma_ojos;
+    int largo_nariz;
+
+    public Nieta(String forma_ojos, int largo_nariz) {
+        this.forma_ojos = forma_ojos;
+        this.largo_nariz = largo_nariz;
+    }
+
+    public String getForma_ojos() {
+        return forma_ojos;
+    }
+
+    public void setForma_ojos(String forma_ojos) {
+        this.forma_ojos = forma_ojos;
+    }
+
+    public int getLargo_nariz() {
+        return largo_nariz;
+    }
+
+    public void setLargo_nariz(int largo_nariz) {
+        this.largo_nariz = largo_nariz;
+    }
 }
